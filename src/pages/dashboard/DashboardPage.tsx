dc048fbde2dd11dc9eacae8e3bcc6bbc35754abe
quyenptt03 <quyenptt03@gmail.com>
2025-04-20 15:58:23 +0700

    fix bug

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import AbnormalDetectionCard from "../../components/card/AbnormalDetectionCard";
 import phvt from "../../assets/phvn.png";
 import phbt1 from "../../assets/phbt1.png";
-import BarnSelector, { Barn } from "../../components/barn-selector/BarnSelector";
+import BarnSelector, {
+  Barn,
+} from "../../components/barn-selector/BarnSelector";
 import { FiChevronDown, FiSearch } from "react-icons/fi";
 import { MdOutlinePets } from "react-icons/md";
 import BarChartComponent from "../../components/bar-chart/BarChartComponent";
 import TimelineSelector from "../../components/timeline-selector/TimelineSelector";
 import LineChartComponent from "../../components/line-chart/LineChartComponent";
+import { generateData } from "../../utils/generateData";
 
 const abnormalDetections = [
   {
@@ -33,31 +36,41 @@ const abnormalDetections = [
   },
 ];
 
-const imageList = [
-  { src: phbt1, caption: "Chuồng Heo01A1" },
-];
+const imageList = [{ src: phbt1, caption: "Chuồng Heo01A1" }];
 const DashBoard = () => {
   const handleBarnSelect = (id: string) => {
     console.log("Selected Barn ID:", id);
   };
-    const [barns, setBarns] = useState<Barn[]>([]);
+  const [barns, setBarns] = useState<Barn[]>([]);
   useEffect(() => {
-      const fetchBarns = async () => {
-        try {
-          const response = await fetch("https://agriculture-traceability.vercel.app/api/v1/rooms");
-          const data = await response.json();
-          setBarns(data.rooms);
-        } catch (error) {
-          console.error("Error fetching:", error);
-        }
-      };
-      fetchBarns();
-    }, []);
-  const [filterType, setFilterType] = useState<"year" | "month" | "week">("year");
+    const fetchBarns = async () => {
+      try {
+        const response = await fetch(
+          "https://agriculture-traceability.vercel.app/api/v1/rooms"
+        );
+        const data = await response.json();
+        setBarns(data.rooms);
+      } catch (error) {
+        console.error("Error fetching:", error);
+      }
+    };
+    fetchBarns();
+  }, []);
+  const [filterType, setFilterType] = useState<"year" | "month" | "week">(
+    "year"
+  );
   const [selectedAnimal, setSelectedAnimal] = useState("");
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
   const [selectedMonth, setSelectedMonth] = useState<number | null>(null);
   const [selectedWeek, setSelectedWeek] = useState<number | null>(null);
+  const [herdData, setHerdData] = useState<
+    { cachly?: number; name: string; khoe: number; benh: number }[]
+  >([]);
+
+  useEffect(() => {
+    const genData = generateData(filterType, true);
+    setHerdData(genData);
+  }, [filterType]);
 
   return (
     <>
@@ -66,8 +79,12 @@ const DashBoard = () => {
           <div className="flex justify-between items-center w-full mb-2">
             <span className="text-left text-black text-lg">Đàn HeoA101S1</span>
             <div className="hidden md:flex items-center gap-2 text-xs rounded-full bg-white border border-[#E0E2E7] px-2 max-w-[340px]">
-              <FiSearch  className="text-[#278D45] w-5 h-5" />
-              <input type="text" placeholder="Tìm kiếm..." className="text-[#737791] text-sm w-[200px] p-2 bg-transparent outline-none"/>
+              <FiSearch className="text-[#278D45] w-5 h-5" />
+              <input
+                type="text"
+                placeholder="Tìm kiếm..."
+                className="text-[#737791] text-sm w-[200px] p-2 bg-transparent outline-none"
+              />
               <FiChevronDown className="text-[#737791] w-5 h-5" />
             </div>
           </div>
@@ -75,10 +92,10 @@ const DashBoard = () => {
           <div className="grid gap-2 z-1 ">
             {imageList.map((image, index) => (
               <div key={index} className="relative">
-               <img
-                src={image.src}
-                alt={`Image ${index + 1}`}
-                className="w-full h-auto object-cover"
+                <img
+                  src={image.src}
+                  alt={`Image ${index + 1}`}
+                  className="w-full h-auto object-cover"
                 />
                 <p className="absolute bottom-2 left-2 bg-gray-400 text-white text-sm px-2 py-1 rounded-md">
                   {image.caption}
@@ -87,17 +104,17 @@ const DashBoard = () => {
             ))}
           </div>
         </div>
-      
+
         <div className="inline-flex flex-col gap-2 bg-white p-3 rounded-[8px]">
-          <BarnSelector 
-              barns={barns}
-              onSelect={handleBarnSelect}
-              icon={<MdOutlinePets className="w-5 h-5" />}
-              rounded={false}
-              placeholder="Đàn hep HA01"
-              iconColor="text-white"
-              iconBgColor="bg-yellow-500"
-            />
+          <BarnSelector
+            barns={barns}
+            onSelect={handleBarnSelect}
+            icon={<MdOutlinePets className="w-5 h-5" />}
+            rounded={false}
+            placeholder="Đàn hep HA01"
+            iconColor="text-white"
+            iconBgColor="bg-yellow-500"
+          />
           <span className="text-left text-black">Số lượng: 10010</span>
           <span className="text-left text-black">Lịch sử hoạt động</span>
           {abnormalDetections.map((alert, index) => (
@@ -122,20 +139,21 @@ const DashBoard = () => {
             setSelectedWeek={setSelectedWeek}
           />
         </div>
-        
+
         <div className="flex flex-col gap-y-5">
-          <LineChartComponent 
-            title="Biểu đồ xu hướng tiêu thụ" 
-            chartType="consumption" 
-            filterType={filterType} 
+          <LineChartComponent
+            title="Biểu đồ xu hướng tiêu thụ"
+            chartType="consumption"
+            filterType={filterType}
           />
-          <LineChartComponent 
-          title="Tăng giảm số lượng tổng thể" 
-          chartType="herd" 
-          filterType={filterType} 
+          <LineChartComponent
+            title="Tăng giảm số lượng tổng thể"
+            chartType="herd"
+            filterType={filterType}
           />
           <BarChartComponent
             title="Tỉ lệ nhập kho"
+            data={herdData}
             filterType={filterType}
             hasIsolation={false}
             selectedAnimal={selectedAnimal}
@@ -146,5 +164,4 @@ const DashBoard = () => {
   );
 };
 
-  export default DashBoard;
-  
+export default DashBoard;
